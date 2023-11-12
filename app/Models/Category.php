740e60a3ab60ee8f3979e609df9286d48016b944
Shahadat-Murshed <shahadat.murshed@gmail.com>
2023-11-12 08:02:00 +0600

    Menu Responsive and Child Category Finished

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    public function subCategories(){
+        return $this->hasMany(SubCategory::class);
+    }
+
+    public function childCategories(){
+        return $this->hasMany(ChildCategory::class);
+    }
 }

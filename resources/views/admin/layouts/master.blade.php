662fcf9d66ebde2b65909ac9ecbd47d0a154e4b5
Shahadat-Murshed <shahadat.murshed@gmail.com>
2024-01-31 20:42:56 +0600

    copyright

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -55,7 +55,7 @@
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+          Copyright &copy; 2023 <div class="bullet"></div><a href="https://github.com/Shahadat-Murshed">Shahadat Mursed</a>
         </div>
         <div class="footer-right">
           

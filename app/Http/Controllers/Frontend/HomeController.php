d4874ae58e5ba953bc52ae68847f6d4c6f9ba17e
Shahadat-Murshed <shahadat.murshed@gmail.com>
2024-05-12 05:21:40 +0600

    Home Page Populized

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,24 +3,37 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use App\Models\FlashSale;
 use App\Models\FlashSaleItem;
+use App\Models\HomePageSetting;
 use App\Models\Product;
 use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $sliders = Slider::where('status', 1)->orderBy('serial', 'asc')->get();
         $flashSaleDate = FlashSale::first();
         $flashSaleItems = FlashSaleItem::where('show_at_home', 1)->where('status', 1)->get();
-        $products = Product::where('status',1)->get();
-        return view('frontend.home.home',
-        compact(
-            'sliders',
-            'flashSaleDate',
-            'flashSaleItems'
-        ));
+        $products = Product::where('status', 1)->get();
+        $popularCategory = HomePageSetting::where('key', 'popular_category_section')->first();
+        $brands = Brand::where('status', 1)->where('is_featured', 1)->get();
+        $categoryProductSliderSectionOne = HomePageSetting::where('key', 'product_slider_section_one')->first();
+        $categoryProductSliderSectionTwo = HomePageSetting::where('key', 'product_slider_section_two')->first();
+        return view(
+            'frontend.home.home',
+            compact(
+                'sliders',
+                'flashSaleDate',
+                'flashSaleItems',
+                'popularCategory',
+                'brands',
+                'categoryProductSliderSectionOne',
+                'categoryProductSliderSectionTwo'
+            )
+        );
     }
 }
